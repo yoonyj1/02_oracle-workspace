ad6adbaab6ac7fa3ba9fc67ce37877be96d699c2
yoonyj1 <yoonyj2410@naver.com>
2022-11-25 15:06:36 +0900

    202211251506
    202211251506

--- 01_DML(SELECT)/99_쿼리공부.sql
+++ 01_DML(SELECT)/99_쿼리공부.sql
@@ -13,3 +13,43 @@ FROM EMPLOYEE;
 SELECT *
 FROM EMPLOYEE
 WHERE BONUS IS NULL AND DEPT_CODE IS NULL;
+
+
+-------------------------------- QUIZ 2 ----------------------------------------
+-- 검색하고자 하는 내용
+-- JOB_CODE가 J7이거나 J6이면서 SALARY 값이 200만원 이상이고 BONUS가 있고, 여자이며, 이메일 주소는 _앞이 3글자만 있는
+-- 사원의 EMP_NAME, EMP_NO, JOB_CODE, DEPT_CODE, SALARY, BONUS를 조회
+-- 정상적으로 조회가 잘 된다면 실행결과는 2행이어야 함
+
+-- 위의 내용을 실행시키고자 작성한 SQL문
+SELECT EMP_NAME, EMP_NO, JOB_CODE, DEPT_CODE, SALARY, BONUS
+FROM EMPLOYEE
+WHERE JOB_CODE = 'J7' OR JOB_CODE = 'J6' AND SALARY > 2000000
+AND EMAIL LIKE '____%' AND BONUS IS NULL;
+
+-- 위의 SQL문 실행 시 원하는 결과가 제대로 조회가 되지 않음
+-- 어떤 문제점이 있는지 모두 찾아서 서술하고 조치한 SQL문 작성
+
+/*문제점:
+1. AND가 OR보다 우선함
+2. SALARY 200만원 이상이어야하지만 SQL문에는 초과로 입력했음
+3. 이메일 주소 앞이 3글자만 있는 사원을 찾으려면 ESCAPE OPTION을 사용해야함
+4. 여자인 조건 생략
+5. 보너스를 받으려면 IS NOT NULL을 사용해야함
+*/
+
+-- 조치한 SQL문
+SELECT EMP_NAME, EMP_NO, JOB_CODE, DEPT_CODE, SALARY, BONUS
+FROM EMPLOYEE
+WHERE (JOB_CODE = 'J7' OR JOB_CODE = 'J6') AND SALARY >= 2000000
+AND EMAIL LIKE '___$_%' ESCAPE '$' AND BONUS IS NOT NULL
+AND SUBSTR(EMP_NO, 8, 1) = '2';
+
+SELECT EMP_NAME, EMP_NO, JOB_CODE, DEPT_CODE, SALARY, BONUS
+FROM EMPLOYEE
+WHERE JOB_CODE IN ('J7', 'J6') AND SALARY >= 2000000
+AND EMAIL LIKE '___$_%' ESCAPE '$' AND BONUS IS NOT NULL
+AND SUBSTR(EMP_NO, 8, 1) IN ('2', '4');
+
+SELECT EMP_NAME, EMAIL, SUBSTR(EMAIL, 1, INSTR(EMAIL, '_')-1), LENGTH(SUBSTR(EMAIL, 1, INSTR(EMAIL, '_')-1))
+FROM EMPLOYEE;
