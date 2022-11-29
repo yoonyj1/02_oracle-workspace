734c1ec7edbe34edde7533a14a77884eb6263081
yoonyj1 <yoonyj2410@naver.com>
2022-11-29 22:12:41 +0900

    202211291012H
    202211291012H

--- WORKBOOK/문제풀이.sql
+++ WORKBOOK/문제풀이.sql
@@ -1,6 +1,7 @@
+-- [Basic SELECT]
+
 -- 1.  춘 기술대학교의 학과 이름과 계열을 표시하시오. 단, 출력 헤더는 "학과 명", "계열" 
 --으로 표시하도록 한다.
-
 SELECT DEPARTMENT_NAME AS "학과 명", CATEGORY AS "계열"
 FROM TB_DEPARTMENT;
 
@@ -53,3 +54,202 @@ FROM TB_DEPARTMENT;
 SELECT STUDENT_NO, STUDENT_NAME, STUDENT_SSN
 FROM TB_STUDENT
 WHERE EXTRACT(YEAR FROM ENTRANCE_DATE) = 2002 AND ABSENCE_YN = 'N' AND SUBSTR(STUDENT_ADDRESS, 1, 2) = '전주';
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+--[Additional SELECT - 함수]
+
+-- 1. 영어영문학과(학과코드 002) 학생들의 학번과 이름, 입학 년도를 입학 년도가 빠른
+--순으로 표시하는 SQL 문장을 작성하시오.( 단, 헤더는 "학번", "이름", "입학년도" 가
+--표시되도록 한다.)
+SELECT STUDENT_NO AS "학번", STUDENT_NAME AS "이름", TO_CHAR(ENTRANCE_DATE, 'YYYY-MM-DD') AS "입학년도"
+FROM TB_STUDENT
+WHERE DEPARTMENT_NO = '002'
+ORDER BY 3;
+
+-- 2. 춘 기술대학교의 교수 중 이름이 세 글자가 아닌 교수가 한 명 있다고 핚다. 그 교수의
+--이름과 주민번호를 화면에 출력하는 SQL 문장을 작성해 보자. (* 이때 올바르게 작성핚 SQL
+--문장의 결과 값이 예상과 다르게 나올 수 있다. 원인이 무엇일지 생각해볼 것)
+SELECT PROFESSOR_NAME, PROFESSOR_SSN
+FROM TB_PROFESSOR
+WHERE NOT PROFESSOR_NAME LIKE '___';
+
+-- 3. 춘 기술대학교의 남자 교수들의 이름과 나이를 출력하는 SQL 문장을 작성하시오. 단
+--이때 나이가 적은 사람에서 많은 사람 순서로 화면에 출력되도록 만드시오. (단, 교수 중
+--2000 년 이후 출생자는 없으며 출력 헤더는 "교수이름", "나이"로 한다. 나이는 ‘만’으로
+--계산한다.)
+SELECT PROFESSOR_NAME AS "교수이름", CASE WHEN SUBSTR(PROFESSOR_SSN, 1, 2) >= 50 THEN EXTRACT(YEAR FROM SYSDATE) - EXTRACT(YEAR FROM TO_DATE(SUBSTR(PROFESSOR_SSN, 1, 6)))
+                                         WHEN SUBSTR(PROFESSOR_SSN, 1, 2) < 50 THEN EXTRACT(YEAR FROM SYSDATE) - (1900 + SUBSTR(PROFESSOR_SSN, 1, 2))
+                                         END AS "나이"
+FROM TB_PROFESSOR
+WHERE SUBSTR(PROFESSOR_SSN, 8, 1) = '1'
+ORDER BY 2;
+
+-- 4. 교수들의 이름 중 성을 제외한 이름한 출력하는 SQL 문장을 작성하시오. 출력 헤더는
+--?이름? 이 찍히도록 핚다. (성이 2 자인 경우는 교수는 없다고 가정하시오)
+SELECT SUBSTR(PROFESSOR_NAME, 2, 2)
+FROM TB_PROFESSOR;
+
+-- 5. 춘 기술대학교의 재수생 입학자를 구하려고 한다. 어떻게 찾아낼 것인가? 이때,
+--19 살에 입학하면 재수를 하지 않은 것으로 간주한다.
+SELECT STUDENT_NO, STUDENT_NAME
+FROM TB_STUDENT
+WHERE EXTRACT(YEAR FROM ENTRANCE_DATE) - EXTRACT(YEAR FROM TO_DATE(SUBSTR(STUDENT_SSN, 1, 6))) + 1 != 20 AND NOT EXTRACT(YEAR FROM ENTRANCE_DATE) - EXTRACT(YEAR FROM TO_DATE(SUBSTR(STUDENT_SSN, 1, 6))) + 1 = 19;
+
+-- 6. 2020 년 크리스마스는 무슨 요일인가?
+SELECT TO_CHAR(TO_DATE(201225), 'DAY') FROM DUAL;
+
+-- 7. TO_DATE('99/10/11','YY/MM/DD'), TO_DATE('49/10/11','YY/MM/DD') 은 각각 몇 년 몇
+--월 몇 일을 의미할까? 또 TO_DATE('99/10/11','RR/MM/DD'),
+--TO_DATE('49/10/11','RR/MM/DD') 은 각각 몇 년 몇 월 몇 일을 의미할까?
+SELECT TO_CHAR(TO_DATE(991011), 'YYYYMMDD') FROM DUAL; -- 1999년 10월 11일
+SELECT TO_CHAR(TO_DATE(491011), 'YYYYMMDD') FROM DUAL; -- 2049년 10월 11일
+SELECT TO_CHAR(TO_DATE(991011), 'RRRR') FROM DUAL; -- 1999년 10월 11일
+SELECT TO_CHAR(TO_DATE(491011), 'RRRR') FROM DUAL; -- 2049년 10월 11일
+
+-- 8. 춘 기술대학교의 2000 년도 이후 입학자들은 학번이 A 로 시작하게 되어있다. 2000 년도
+--이전 학번을 받은 학생들의 학번과 이름을 보여주는 SQL 문장을 작성하시오.
+SELECT STUDENT_NO, STUDENT_NAME
+FROM TB_STUDENT
+WHERE SUBSTR(STUDENT_NO, 1, 1) != 'A';
+
+-- 9. 학번이 A517178 인 한아름 학생의 학점 총 평점을 구하는 SQL 문을 작성하시오. 단,
+--이때 출력 화면의 헤더는 "평점" 이라고 찍히게 하고, 점수는 반올림하여 소수점 이하 한
+--자리까지만 표시한다.
+SELECT ROUND(AVG(POINT), 1) AS "평점"
+FROM TB_GRADE
+WHERE STUDENT_NO = 'A517178'
+GROUP BY SUBSTR(TERM_NO, 1, 3);
+
+--10. 학과별 학생수를 구하여 "학과번호", "학생수(명)" 의 형태로 헤더를 만들어 결과값이
+--출력되도록 하시오
+SELECT DEPARTMENT_NO AS "학과번호", COUNT(*)
+FROM TB_STUDENT
+GROUP BY DEPARTMENT_NO
+ORDER BY 1;
+
+-- 11. 지도 교수를 배정받지 못한 학생의 수는 몇 명 정도 되는 알아내는 SQL 문을
+--작성하시오.
+SELECT COUNT(*)
+FROM TB_STUDENT
+GROUP BY COACH_PROFESSOR_NO
+HAVING COACH_PROFESSOR_NO IS NULL;
+
+-- 12. 학번이 A112113 인 김고운 학생의 년도 별 평점을 구하는 SQL 문을 작성하시오. 단,
+--이때 출력 화면의 헤더는 "년도", "년도 별 평점" 이라고 찍히게 하고, 점수는 반올림하여
+--소수점 이하 한 자리까지만 표시한다.
+SELECT SUBSTR(TERM_NO, 1, 4) AS "년도", ROUND(AVG(POINT), 1) AS "년도 별 평점"
+FROM TB_GRADE
+WHERE STUDENT_NO = 'A112113'
+GROUP BY SUBSTR(TERM_NO, 1, 4)
+ORDER BY 1;
+
+-- 13. 학과 별 휴학생 수를 파악하고자 한다. 학과 번호와 휴학생 수를 표시하는 SQL 문장을
+--작성하시오.
+SELECT DEPARTMENT_NO AS "학과코드명", COUNT(*) AS "휴학생 수"
+FROM TB_STUDENT
+WHERE ABSENCE_YN = 'Y'
+GROUP BY DEPARTMENT_NO
+ORDER BY 1;
+
+-- 14. 춘 대학교에 다니는 동명이인(同名異人) 학생들의 이름을 찾고자 한다. 어떤 SQL
+--문장을 사용하면 가능하겠는가?
+SELECT STUDENT_NAME AS "동명이름", COUNT(*) AS "동명인 수"
+FROM TB_STUDENT
+GROUP BY STUDENT_NAME
+HAVING NOT COUNT(*) = 1
+ORDER BY 1;
+
+-- 15. 학번이 A112113 인 김고운 학생의 년도, 학기 별 평점과 년도 별 누적 평점 , 총
+--평점을 구하는 SQL 문을 작성하시오. (단, 평점은 소수점 1 자리까지만 반올림하여
+--표시한다.)
+SELECT NVL(SUBSTR(TERM_NO, 1, 4), ' ') AS "년도", NVL(SUBSTR(TERM_NO, 5, 2), ' ') AS "학기", ROUND(AVG(POINT), 1) AS "평점"
+FROM TB_GRADE
+WHERE STUDENT_NO = 'A112113'
+GROUP BY ROLLUP(SUBSTR(TERM_NO, 1, 4), SUBSTR(TERM_NO, 5, 2))
+ORDER BY SUBSTR(TERM_NO, 1, 4);
+
+-- [Additional SELECT - Option] ----------------------------
+-- 1.  학생이름과 주소지를 표시하시오. 단, 출력 헤더는 "학생 이름", "주소지"로 하고,
+--정렬은 이름으로 오름차순 표시하도록 한다.
+SELECT STUDENT_NAME AS "학생 이름" , STUDENT_ADDRESS AS "주소지"
+FROM TB_STUDENT
+ORDER BY 1;
+
+-- 2. 휴학중인 학생들의 이름과 주민번호를 나이가 적은 순서로 화면에 출력하시오.
+SELECT STUDENT_NAME, STUDENT_SSN
+FROM TB_STUDENT
+WHERE ABSENCE_YN = 'Y';
+
+-- 3. 주소지가 강원도나 경기도인 학생들 중 1900 년대 학번을 가진 학생들의 이름과 학번,
+--주소를 이름의 오름차순으로 화면에 출력하시오. 단, 출력헤더에는 "학생이름","학번",
+--"거주지 주소" 가 출력되도록 한다.
+SELECT STUDENT_NAME AS "학생이름", STUDENT_NO AS "학번", STUDENT_ADDRESS AS "거주지 주소" 
+FROM TB_STUDENT
+WHERE (STUDENT_ADDRESS LIKE '경기도%' OR STUDENT_ADDRESS LIKE '강원%') AND STUDENT_NO LIKE '9%'
+ORDER BY 1;
+
+-- 4. 현재 법학과 교수 중 가장 나이가 맋은 사람부터 이름을 확인핛 수 있는 SQL 문장을
+--작성하시오. (법학과의 '학과코드'는 학과 테이블(TB_DEPARTMENT)을 조회해서 찾아
+--내도록 하자)005
+SELECT PROFESSOR_NAME, PROFESSOR_SSN
+FROM TB_PROFESSOR
+WHERE DEPARTMENT_NO = '005'
+ORDER BY 2;
+
+-- 5. 2004 년 2 학기에 'C3118100' 과목을 수강핚 학생들의 학점을 조회하려고 핚다. 학점이
+--높은 학생부터 표시하고, 학점이 같으면 학번이 낮은 학생부터 표시하는 구문을
+--작성해보시오.
+SELECT STUDENT_NO, TO_CHAR(POINT, '99.99')
+FROM TB_GRADE
+WHERE CLASS_NO = 'C3118100' AND TERM_NO = '200402'
+ORDER BY 2 DESC;
+
+-- 6. 학생 번호, 학생 이름, 학과 이름을 학생 이름으로 오름차순 정렬하여 출력하는 SQL
+--문을 작성하시오.
+SELECT STUDENT_NO, STUDENT_NAME, DEPARTMENT_NAME
+FROM TB_STUDENT
+JOIN TB_DEPARTMENT USING(DEPARTMENT_NO)
+ORDER BY 2;
+
+-- 7. 춘 기술대학교의 과목 이름과 과목의 학과 이름을 출력하는 SQL 문장을 작성하시오.
+SELECT CLASS_NAME, DEPARTMENT_NAME
+FROM TB_CLASS
+JOIN TB_DEPARTMENT USING (DEPARTMENT_NO);
+
+-- 8. 과목별 교수 이름을 찾으려고 핚다. 과목 이름과 교수 이름을 출력하는 SQL 문을
+--작성하시오.
+SELECT DISTINCT CLASS_NAME, PROFESSOR_NAME
+FROM TB_CLASS_PROFESSOR
+JOIN TB_CLASS USING(CLASS_NO)
+JOIN TB_PROFESSOR USING(DEPARTMENT_NO);
+
+-- 9. 8 번의 결과 중 ‘인문사회’ 계열에 속핚 과목의 교수 이름을 찾으려고 핚다. 이에
+--해당하는 과목 이름과 교수 이름을 출력하는 SQL 문을 작성하시오.
+SELECT DISTINCT CLASS_NAME, PROFESSOR_NAME
+FROM TB_CLASS
+JOIN TB_PROFESSOR USING(DEPARTMENT_NO)
+WHERE DEPARTMENT_NO BETWEEN '001' AND '021';
+
+-- 10.  ‘음악학과’ 학생들의 평점을 구하려고 핚다. 음악학과 학생들의 "학번", "학생 이름",
+--"젂체 평점"을 출력하는 SQL 문장을 작성하시오. (단, 평점은 소수점 1 자리까지맊
+-- 반올림하여 표시핚다.)
+SELECT STUDENT_NO AS "학번", STUDENT_NAME AS "학생 이름", ROUND(AVG(POINT),1) AS "전체 평점"
+FROM TB_GRADE
+JOIN TB_STUDENT USING(STUDENT_NO)
+JOIN TB_DEPARTMENT USING(DEPARTMENT_NO)
+WHERE DEPARTMENT_NO = '059'
+GROUP BY STUDENT_NO, STUDENT_NAME
+ORDER BY 1;
+
+-- 11. 학번이 A313047 인 학생이 학교에 나오고 있지 않다. 지도 교수에게 내용을 젂달하기
+--위핚 학과 이름, 학생 이름과 지도 교수 이름이 필요하다. 이때 사용핛 SQL 문을
+--작성하시오. 단, 출력헤더는 ?학과이름?, ?학생이름?, ?지도교수이름?으로
+--출력되도록 핚다.
+SELECT DEPARTMENT_NAME AS "학과이름", STUDENT_NAME AS "학생이름", PROFESSOR_NAME AS "지도교수이름"
+FROM TB_STUDENT
+JOIN TB_PROFESSOR USING(COACH_PROFESSOR_NO)
+JOIN TB_DEPARTMENT USING(DEPARTMENT_NO)
+WHERE STUDENT_NO = 'A313047';
+
+
